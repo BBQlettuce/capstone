f4df762612c257c5c6ac58d906bd08dc8ef3fb64
Oscar Liu <oscar.liu.13q@gmail.com>
2015-09-15 17:41:30 -0400

    add more .keep files

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#main'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:show]
+    resources :jobs, only: [:show]
   end
 
   resources :users, only: [:new, :create, :show]
