19b9c144b6afa4573f80a05a7173d022a0d8308c
Oscar Liu <oscar.liu.13q@gmail.com>
2015-09-18 17:20:40 -0400

    can delete jobs

--- app/assets/javascripts/views/jobs/job_show.js
+++ app/assets/javascripts/views/jobs/job_show.js
@@ -1,6 +1,12 @@
 Indoge.Views.JobShow = Backbone.View.extend({
   template: JST["jobs/job_show"],
 
+  events: {
+    "click .save-link": "saveJob",
+    "click .unsave-link": "unsaveJob",
+    "click .delete-job-link": "deleteJob"
+  },
+
   initialize: function(options) {
     this.job = options.job;
     this.listenTo(this.job, "sync", this.render);
@@ -9,7 +15,20 @@ Indoge.Views.JobShow = Backbone.View.extend({
   render: function() {
     this.$el.html(this.template({job: this.job}));
     return this;
-  }
+  },
 
+  saveJob: function() {
+
+  },
+
+  unsaveJob: function() {
+
+  },
+
+  deleteJob: function() {
+    this.job.destroy();
+    this.remove();
+    Backbone.history.navigate("hire", {trigger: true});
+  }
 
 })
