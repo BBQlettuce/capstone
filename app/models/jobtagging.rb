2e73ea513e3f40d7bebf12a00b1eda2eca8f4aa7
Oscar Liu <oscar.liu.13q@gmail.com>
2015-09-22 22:47:25 -0400

    bugfix to get heroku working

--- app/models/jobtagging.rb
+++ app/models/jobtagging.rb
@@ -1,5 +1,5 @@
 class Jobtagging < ActiveRecord::Base
-  validates :job_id, :tag_id, :presence: true
+  validates :job_id, :tag_id, presence: true
   validates :job_id, uniqueness: {scope: :tag_id}
 
   belongs_to :job
