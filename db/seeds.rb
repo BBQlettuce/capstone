3e17dac969efa5748cc877051614515920e555be
Oscar Liu <oscar.liu.13q@gmail.com>
2015-09-24 18:06:14 -0400

    add tons of seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,19 @@
 
 u1 = User.create!(email: "bananas@hoohah.com", name: "Diddy", password: "password")
 u2 = User.create!(email: "doge@doge.com", name: "DHDoge", password: "password")
-u3 = User.create!(email: "expand@dong.com", name: "DK", password: "password")
+u3 = User.create!(email: "expand@dong.com", name: "Dogey Kong", password: "password")
 u4 = User.create!(email: "spamdoge@doge.com", name: "SpamDoge", password: "password")
+u5 = User.create!(email: "lonelydoge@doge.com", name: "Shiba", password: "password")
 
 u1.posted_jobs.create!(title: "Duck Hunter",
   description: "Seeking a doge who can hunt ducks. " +
-  "At least 5 years of experience required. Treats negotiable.")
-u1.create_resume!(text: "my private resume of downthrow upair", private: true)
-u2.create_resume!(text: "25 years of experience hunting ducks.")
-u3.posted_jobs.create!(title: "expand", description: "ding dong")
+  "At least 5 years of experience required." +
+  "Must be comfortable with firearms and rowdy gentlemen. Treats negotiable.",
+  salary: 22)
+u1.create_resume!(text: "my private resume", private: true)
+u2.create_resume!(text: "25 years of experience hunting ducks. Proficient at all kinds of firearms." +
+  "Looking to improve my hunting skills in the wilderness.")
+u3.posted_jobs.create!(title: "Clean my drums", description: "Please clean my drums. Part-time.", salary: 6)
 u3.create_resume!(text:
   "He's the leader of the bunch, you know him well \n" +
   "He's finally back, to kick some tail \n" +
@@ -24,14 +28,21 @@ u3.create_resume!(text:
   "If he shoots ya, its gonna hurt \n" +
   "He's bigger, faster, and stronger too \n" +
   "He's the first member, of the DK Crew!")
+u5.posted_jobs.create!(title: "Doge walker",
+  description: "Position open for doge walker or doge companion." +
+    "Required skills: barkchelor's degree in walking and hiking" +
+    "Much fun, very streets, wow",
+  salary: 13)
 
 25.times do |i|
   u4.posted_jobs.create!(title: "Totally legit job ##{i}",
-    description: "Make easy treats without any effort. This is not a scam. Add some more lines to make it look longer. Hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue hue. "
+    description: "Make easy treats without any effort. This is not a scam. Over 200 treats per week and unlimited belly rubs and walks." +
+      "Make easy treats without any effort. This is not a scam. Over 200 treats per week and unlimited belly rubs and walks.",
+    salary: 200
   )
 end
 
 25.times do |i|
-  u = User.create!(email: "bot#{i}", name: "bot#{i}", password: "password")
-  u.create_resume!(text: "i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot. i am a bot.")
+  u = User.create!(email: "catbot#{i}", name: "catbot#{i}", password: "password")
+  u.create_resume!(text: "i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat. i am a cat.")
 end
